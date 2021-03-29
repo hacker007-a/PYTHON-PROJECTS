8796c7bc64879d8c08c63987f01b8bb3a58b4cbb
hacker007-a <77221468+hacker007-a@users.noreply.github.com>
2021-03-29 12:23:05 +0530

    Add files via upload

---
+++ practical/Python Practice.py
@@ -0,0 +1,46 @@
+# # Function and docstring
+# def function1(a, b):
+#     '''this is the function calculate the 
+#     average of two numbers'''
+#     avg = (a+b)/2
+#     # print(avg)
+#     return avg
+
+# v = function1(5, 7)
+# print(v)
+
+# print(function1.__doc__)  # important
+#----------------------------------------------------------#
+
+# Try, except , exception handling
+
+# num1 = input('Enter num1: ')
+# num2 = input('enter num2: ')
+# try:
+#     print('The sum of two num is ', int(num1)+int(num2))
+
+
+# except Exception as e:
+#     print("Please enter a number ")
+
+# print('this is imp')
+
+#---------------------------------------------------------------------------
+
+
+#----------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
