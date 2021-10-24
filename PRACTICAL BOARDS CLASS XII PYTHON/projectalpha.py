88e44add22247f83f6278a94cb625eedadedc237
hacker007-a <77221468+hacker007-a@users.noreply.github.com>
2021-10-24 09:48:06 +0530

    Update projectalpha.py

--- PRACTICAL BOARDS CLASS XII PYTHON/projectalpha.py
+++ PRACTICAL BOARDS CLASS XII PYTHON/projectalpha.py
@@ -33,54 +33,54 @@
 
 #Create a binary file with roll no, name, and marks. input a roll number and update the marks.
 
-from os import close, truncate
-import pickle
-def write():
-    f = open('studentdetails.dat', 'wb')
-    while True:
-        r = int(input('Enter Roll no.: '))
-        n = input('Enter Name: ')
-        m = int(input('Enter marks: '))
-        file = [r,n,m]
-        pickle.dump(file, f)
-        ch = input('Do you want to enter more?(Y/N): ')
-        if ch in 'Nn':
-            break
-    f.close()
+# from os import close, truncate
+# import pickle
+# def write():
+#     f = open('studentdetails.dat', 'wb')
+#     while True:
+#         r = int(input('Enter Roll no.: '))
+#         n = input('Enter Name: ')
+#         m = int(input('Enter marks: '))
+#         file = [r,n,m]
+#         pickle.dump(file, f)
+#         ch = input('Do you want to enter more?(Y/N): ')
+#         if ch in 'Nn':
+#             break
+#     f.close()
 
-def read():
-    f = open('studentdetails.dat', 'rb')
-    try:
-        while True:
-            rec = pickle.load(f)
-            print(rec)
-    except EOFError:
-        f.close()
+# def read():
+#     f = open('studentdetails.dat', 'rb')
+#     try:
+#         while True:
+#             rec = pickle.load(f)
+#             print(rec)
+#     except EOFError:
+#         f.close()
 
-def update():
-    f = open('studentdetails.dat', 'rb+')
-    rollno = int(input('Enter roll no. whose marks you want to update: '))
-    found = 0
-    try:
+# def update():
+#     f = open('studentdetails.dat', 'rb+')
+#     rollno = int(input('Enter roll no. whose marks you want to update: '))
+#     found = 0
+#     try:
         
-        while True:
-            pos = f.tell()
-            rec = pickle.load(f)
-            if rec[0] == rollno:
-                um = int(input('Enter updates marks: ')) 
-                rec[2]=um
-                f.seek(pos)
-                pickle.dump(rec,f)
-                found = 1
-                print('Record Updated.')
+#         while True:
+#             pos = f.tell()
+#             rec = pickle.load(f)
+#             if rec[0] == rollno:
+#                 um = int(input('Enter updates marks: ')) 
+#                 rec[2]=um
+#                 f.seek(pos)
+#                 pickle.dump(rec,f)
+#                 found = 1
+#                 print('Record Updated.')
                 
-    except EOFError:
-        f.close()
-    if found == 0:
-        print('Record not found...')
+#     except EOFError:
+#         f.close()
+#     if found == 0:
+#         print('Record not found...')
 
 
-# write()
-read()
-update()
-read()
+# # write()
+# read()
+# update()
+# read()
